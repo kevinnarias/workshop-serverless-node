e61b274f7db9e18b9f9c68ef1066ff9c37cc8fc3
Kevin Arias <ingkevinarias@gmail.com>
2022-04-29 11:34:38 -0500

    Update entrypoint.js

--- app/handler/entrypoint.js
+++ app/handler/entrypoint.js
@@ -2,5 +2,5 @@
 const { generateResponse } = require ('../utils/utils')
 
 module.exports.hello = async (event) => {
-  return generateResponse("Hola Tres")
+  return generateResponse("Hola Tres Pepes")
 };
